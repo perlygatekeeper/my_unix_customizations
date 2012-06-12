47e02fd72a52b33a4f6edac4613c43e23a843300
Steven Parker <parker.12@osu.edu>
2012-06-12 14:21:37 -0400

    add some smarts concerning history to tcshrc2bash_aliases and made .bashrc and .bash_aliases, exanded .completion from http://hea-www.harvard.edu/~fine/Tech/tcsh.html

--- bin/tcshrc2bash_aliases.pl
+++ bin/tcshrc2bash_aliases.pl
@@ -7,9 +7,11 @@
 use strict;
 use warnings;
 
-my $name = $0; $name =~ s'.*/''; # remove path--like basename
-my $usage = "usage:\n$name [-opt1] [-opt2] [-opt3]";
+  my $name = $0; $name =~ s'.*/''; # remove path--like basename
+  my $usage = "usage:\n$name [-opt1] [-opt2] [-opt3]";
 
+  my $histsize;
+  my $histfilesize;
 
   my $infile  = ".tcshrc";
   my $outfile = ".bash_aliases";
@@ -17,24 +19,28 @@ my $usage = "usage:\n$name [-opt1] [-opt2] [-opt3]";
   open(OUTFILE,">$outfile") || die("$name: Cannot write to '$outfile': $!\n");
 
 
-  my $history_lines;
-  $history_lines = << '  EOF';
-  HISTCONTROL=erasedups:ignorespace
-  # append to the history file, don't overwrite it
-  shopt -s histappend
-  HISTSIZE=2048
-  HISTFILESIZE=2048
-
-  EOF
-  $history_lines =~ s/^\s{2}//gm;
-  print OUTFILE $history_lines;
-
   while (<INFILE>) {
+    if ( /^[^#]*history\s*=\s*(\d+)/     ) { print OUTFILE "HISTSIZE=$1\n";     $histsize++;}
+    if ( /^[^#]*savehist\s*=[\s(]*(\d+)/ ) { print OUTFILE "HISTFILESIZE=$1\n"; $histfilesize++;}
     next unless ( /alias|^a / );
 	s/^a (\S+)\s+(\S.*)/sprintf "a %10s = %s", $1, $2/e;
 	s/^alias\s+(\S+)\s+(\S.*)/sprintf "alias %6s = %s", $1, $2/e;
     print OUTFILE;
   }
+
+  my $history_lines;
+  $history_lines = << '  EOF';
+  HISTCONTROL=erasedups:ignorespace
+  # append to the history file, don't overwrite it
+  shopt -s histappend
+  
+  EOF
+  $history_lines =~ s/^\s{2}//gm;
+  print OUTFILE "HISTSIZE=2048\n"     if (not $histsize);
+  print OUTFILE "HISTFILESIZE=2048\n" if (not $histfilesize);
+  print OUTFILE $history_lines;
+
+  print OUTFILE qq(bind '"\\ep": history-search-backward'\n);
 
   close(OUTFILE);
   close(INFILE);
