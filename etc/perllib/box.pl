17b2291a5b4705babb30e99979d5f7c3ab08e712
Steven Parker <sparker@manta.com>
2013-05-29 17:06:15 -0400

    small change for manta, git stuff, and perllib cleanup

--- etc/perllib/box.pl
+++ etc/perllib/box.pl
@@ -1,19 +1,19 @@
 #!/usr/bin/curseperl
 
-my $VERT = unpack('c', '|');	# or 0 if supported
-my $HORZ = unpack('c', '-');	# ditto
+my $vert = unpack('c', '|');	# or 0 if supported
+my $horz = unpack('c', '-');	# ditto
 
-sub BOX {
+sub box {
   my($win) = @_;
   $win=&newwin(10,10,2,2) if (!$win);
-  box($win,$VERT,$HORZ);
+  box($win,$vert,$horz);
   wrefresh($win);
 
   $win;				# this returns $win as the value
 }
 
 initscr();
-$newwin = &BOX(0);		# create a new window
+my $newwin = &box(0);		# create a new window
 wmove($newwin, 3, 3);
 waddstr($newwin, "hi");	# add some text
 wrefresh($newwin);
