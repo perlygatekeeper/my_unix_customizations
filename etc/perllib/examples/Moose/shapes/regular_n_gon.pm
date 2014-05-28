792571b0362ab3d8e4b07550dd6f8af3762d3745
Steven Parker <sparker@manta.com>
2014-05-28 19:48:38 -0400

    work on Moose shapes example

--- etc/perllib/examples/Moose/shapes/regular_n_gon.pm
+++ etc/perllib/examples/Moose/shapes/regular_n_gon.pm
@@ -5,13 +5,13 @@ use Math::Trig ':pi';
 
 # print pi2 . "\n";
  
-has 'sides'          => ( isa => 'Int', is => 'rw', required => 1 );
-has 'internal_angle' => ( isa => 'Num', is => 'rw', required => 0, lazy =>1, builder=>'_find_internal_angle' );
+has 'sides'          => ( isa => 'Int', is => 'ro', required => 1, );
+has 'internal_angle' => ( isa => 'Num', is => 'ro', required => 0, lazy =>1, builder=>'_find_internal_angle' );
  
 sub _find_internal_angle {
     my $self = shift;
 	return 0 if ( not $self->sides );
-	return pi2/$self->sides;
+	return pi - pi2/$self->sides;
 }
 
 sub in_degrees {
--- etc/perllib/examples/Moose/shapes/sized_regular_n_gon.pm
+++ etc/perllib/examples/Moose/shapes/sized_regular_n_gon.pm
@@ -1,44 +1,89 @@
 # now we give it a size
-package sized_regualr_n_gon;
+package sized_regular_n_gon;
+use Math::Trig;
 use Moose;
 
-BEGIN { extends 'regular_n_gon' }
+extends 'regular_n_gon';
 
-has 'super_radius' => ( isa => 'Num', is => 'rw', lazy=>1, required=>0, trigger=>\&set_sub_n_side_from_super() ); # radius of superscribed circle
-has 'sub_radius'   => ( isa => 'Num', is => 'rw', lazy=>1, required=>0, trigger=>\&set_super_n_side_from_sub() ); # radius of subcribed circle
-has 'side_length'  => ( isa => 'Num', is => 'rw', lazy=>1, required=>0, trigger=>\&set_sub_n_super_from_side() );
- 
-sub perimeter {
+has 'super_radius' => (
+  isa => 'Num', is => 'rw',
+  clearer => '_clear_super_radius', predicate => '_has_super_radius',
+  lazy => 1, builder => '_build_super_radius',
+  # trigger => sub { $_[0]->_clear_sub_radius; $_[0]->_clear_side_length },
+);
+
+sub _build_super_radius {
+  my ($self) = @_;
+  if ($self->_has_sub_radius) {
+    return $self->sub_radius       / sin( $self->internal_angle/2.0 );
+  } elsif ($self->_has_side_length) {
+    return $self->side_length/2.0  / cos( $self->internal_angle/2.0 );
+  } else {
+    die "Trying to infer super_radius but neither sub_radius nor side_length is set";
+  }
+}
+
+has 'sub_radius' => (
+  isa => 'Num', is => 'rw',
+  clearer => '_clear_sub_radius', predicate => '_has_sub_radius',
+  lazy => 1, builder => '_build_sub_radius',
+  # trigger => sub { $_[0]->_clear_sub_radius; $_[0]->_clear_side_length },
+);
+
+sub _build_sub_radius {
+  my ($self) = @_;
+  if ($self->_has_super_radius) {
+    return $self->super_radius     * sin( $self->internal_angle/2.0 );
+  } elsif ($self->_has_side_length) {
+    return $self->side_length/2.0  * tan( $self->internal_angle/2.0 );
+  } else {
+    die "Trying to infer sub_radius but neither super_radius nor side_length is set";
+  }
+}
+
+has 'side_length' => (
+  isa => 'Num', is => 'rw',
+  clearer => '_clear_side_length', predicate => '_has_side_length',
+  lazy => 1, builder => '_build_side_length',
+  # trigger => sub { $_[0]->_clear_side_length; $_[0]->_clear_side_length },
+);
+
+sub _build_side_length {
+  my ($self) = @_;
+  if ($self->_has_super_radius) {
+    return 2.0 * $self->super_radius * cos( $self->internal_angle/2.0 );
+  } elsif ($self->_has_sub_radius) {
+    print $self->internal_angle/2.0 . "\n";
+    print tan($self->internal_angle/2.0) . "\n";
+    return 2.0 * $self->sub_radius   / tan( $self->internal_angle/2.0 );
+  } else {
+    die "Trying to infer side_length but neither super_radius nor sub_radius is set";
+  }
+}
+
+has 'perimeter' => (
+  isa => 'Num', is => 'rw',
+  clearer => '_clear_perimeter', predicate => '_has_perimeter',
+  lazy => 1, builder => '_build_perimeter',
+);
+
+sub _build_perimeter {
     my $self = shift;
 	return $self->sides() * $self->side_length();
 }
 
-sub area {
+has 'area' => (
+  isa => 'Num', is => 'rw',
+  clearer => '_clear_area', predicate => '_has_area',
+  lazy => 1, builder => '_build_area',
+);
+
+sub _build_area {
     my $self = shift;
 	# number_of_triangles * ( 1/2 * Base * Height)
-	return $self->sides() * $self->side_length() * $self->sub_radius() / 2.0;
+	return $self->sides * $self->side_length/2.0 * $self->sub_radius;
 }
 
-sub set_sub_n_side_from_super {
-	my ( $self, $super, $old_super) = @_;
-	$self->{sub_radius}   = $super * cos( $self->internal_angle );
-	$self->{side_length}  = $super * sin( $self->internal_angle );
-}
-
-sub set_super_n_side_from_sub {
-	my ( $self, $sub, $old_sub) = @_;
-	$self->{super_radius} = $sub / cos( $self->internal_angle );
-	$self->{side_length}  = $sub / tan( $self->internal_angle );
-}
-
-sub set_sub_n_super_from_side {
-	my ( $self, $side, $old_side) = @_;
-	$self->{sub_radius}   = $side * tan( $self->internal_angle );
-	$self->{super_radius} = $side / cos( $self->internal_angle );
-}
- 
-# now we give it an angle
-# now we give it an origin
 
 1;
 
@@ -60,14 +105,14 @@ __END__
 	  hSide          hSide = Side / 2
 
 
-  hSide = sin(a)    hSide = sin(a) * Super    Super = hSide/sin(a)
+  hSide = cos(a)    hSide = cos(a) * Super    Super = hSide/cos(a)
   Super
 
-  _Sub_ = cos(a)      Sub = cos(a) * Super    Super = Sub/cos(a)
+  _Sub_ = sin(a)      Sub = sin(a) * Super    Super = Sub/sin(a)
   Super
 
   _Sub_ = tan(a)      Sub = tan(a) * hSide    hSide = Sub/tan(a)
   hSide
 
 
-
+    return $self->sub_radius       / sin( $self->internal_angle/2.0 );
