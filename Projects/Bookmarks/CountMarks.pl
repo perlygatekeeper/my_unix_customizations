6c8539aad5b967797c418f2042ae662f658ded93
Steven Parker <sparker@manta.com>
2014-11-12 16:53:24 -0500

    restart count bookmarks project

---
+++ Projects/Bookmarks/CountMarks.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/env perl
+# A perl script to count bookmarks from Chrome
+
+my $name = $0; $name =~ s'.*/''; # remove path--like basename
+my $usage = "usage:\n$name [-opt1] [-opt2] [-opt3]";
+
+use strict;
+use warnings;
+
+exit 0;
+
+__END__
