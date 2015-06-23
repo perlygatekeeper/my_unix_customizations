aaecdbb0100019c22297829bf48dfe02db46e915
Steven Parker <parks17@nationwide.com>
2015-06-23 10:32:44 -0400

    work on getopt-03.pl example file

--- etc/examples/GetOptsLong/getopt-03.pl
+++ etc/examples/GetOptsLong/getopt-03.pl
@@ -14,17 +14,18 @@ my $usage = "usage:\n$name";
 # A negatable option is specified with an exclamation mark "!" after the option name:
 
 use Getopt::Long 2.34;  # $version > 2.32 gives --help - and --version for
+use Data::Dumper;
 use Pod::Usage;
 
 $main::VERSION = 3.1;
-print STDERR "main::VERSION: ($main::VERSION)\n"; 
+# print STDERR "main::VERSION: ($main::VERSION)\n"; 
 
 # Defaults
-my $verbosity = 0;
+my $verbosity = 0;                           # set up connectioned defaults
 my $help      = 0;
 my $debug     = 0;
 my $man       = 0;
-my $opts = { 'infile'      => "message.opt",
+my $opts = { 'infile'      => "message.opt", # set up connections and defaults
              'outfile'     => "message.txt",
              'verbosity'   => \$verbosity,
              'help'        => \$help,
@@ -34,6 +35,8 @@ my $opts = { 'infile'      => "message.opt",
 GetOptions ( $opts, 'outfile:s', "infile:s", "verbosity+", "debug_level+", 'help|?', 'man', 'version' ) or pod2usage(2);
 pod2usage(1) if $help;
 pod2usage(-exitval => 0, -verbose => 2) if $man;
+
+print Dumper($opts);
 
 exit 0;
 
@@ -48,8 +51,12 @@ sample - Using Getopt::Long and Pod::Usage
 sample [options] [file ...]
 
 Options:
-  -help            brief help message
+  -help|-?         brief help message
   -man             full documentation
+  -outfile         output filename
+  -infile          input filename
+  -debug_level     level of debugging, useful range 0..6
+  -verbosity       call multiple time to up the amount of info printed
 
 =head1 OPTIONS
 
